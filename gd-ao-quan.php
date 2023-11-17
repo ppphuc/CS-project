a915b18c3303ce84167d4eaebd44a56c9c54d652
NgocLe21 <151150175+NgocLe21@users.noreply.github.com>
2023-11-17 19:42:01 +0700

    Add files via upload

---
+++ gd-ao-quan.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="gd.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <title>CS Shop</title>
+</head>
+<body>
+  <div class="outfit-container">
+    <div class="outfit">
+      <img src="images/jean.jpg" alt="Brown Leather Jacket">
+      <p>Quần jean xanh</p>
+      <tr>500.000k</tr>
+      <p><span class="heart-icon"></i></span></p>
+    </div>
+    <div class="outfit">
+      <img src="images/jean.jpg" alt="Quần jean xanh" title="Quần jean xanh">
+      <p>Quần jean xanh</p>
+      <tr>500.000k</tr>
+      <p><span class="heart-icon"></span></p>
+    </div>
+    <div class="outfit">
+      <img src="images/jean.jpg" alt="Quần jean xanh" title="Quần jean xanh">
+      <p>Quần jean xanh</p>
+      <tr>500.000k</tr>
+      <p><span class="heart-icon"></span></p>
+    </div>
+    <div class="outfit">
+      <img src="images/jean.jpg" alt="Quần jean xanh" title="Quần jean xanh">
+      <p>Quần jean xanh</p>
+      <tr>500.000k</tr>
+      <p><span class="heart-icon"></span></p>
+    </div>
+    <div class="outfit">
+      <img src="images/jean.jpg" alt="Quần jean xanh" title="Quần jean xanh">
+      <p>Quần jean xanh</p>
+      <tr>500.000k</tr>
+      <p><span class="heart-icon"></span></p>
+    </div>
+    <div class="outfit">
+      <img src="images/jean.jpg" alt="Quần jean xanh" title="Quần jean xanh">
+      <p>Quần jean xanh</p>
+      <tr>500.000k</tr>
+      <p><span class="heart-icon"></span></p>
+    </div>
+  </div>
+  <div class="pagination">
+    <a href="">&laquo; Previous</a>
+    <a href="" class="active">1</a>
+    <a href="">2</a>
+    <a href="">3</a>
+    <a href="">Next &raquo;</a>
+  </div>
+</body>
+</html>
